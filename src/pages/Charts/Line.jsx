bbced898699473d17a15796b2edfd6dc2770d751
Abhishek Kumar <abhishek02102003@gmail.com>
2023-02-12 00:18:23 +0530

    chart updated

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -5,7 +5,7 @@ import { ChartHeader, LineChart } from '../../components'
 
 const Line = () => {
   return (
-    <div className='m-4 md:m-10 w-full mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
       <ChartHeader category='Chart' title="Inflation Rate" />
 
       <div className="w-full">
